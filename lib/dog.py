4e925a9e1e61f845f9991daa4a338e70c7f0c884
kwamebrowne <theeecryptoking@gmail.com>
2023-05-24 14:21:25 -0500

    i hate this lab

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,61 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+# class Dog:
+#     def __init__(self, name, breed):
+#         self.name = name
+#         self.breed = breed
+
+#     def get_name(self):
+#         return self._name
+    
+#     def set_name(self, name):
+#         if ((isinstance(name,  str)) and (0 <= len(name) <=25)):
+#             print(f"Setting name to {name}")
+#             self._name = name
+#         else :
+#             print("Name must be string between 1 and 25 characters. ")
+
+#     name = property(get_name, set_name)
+
+#     def get_breed(self):
+#         return self._breed
+    
+#     def set_breed(self, breed):
+#         if breed in APPROVED_BREEDS:
+#             self._breed = breed
+#         else:
+#             print("Breed must be in list of approved breeds.")
+
+#     # name = property(get_name, set_name, get_breed, set_breed)
+#     breed = property(get_breed, set_breed)
+
+# Solution code from school 
 class Dog:
-    pass
+    def __init__(self, name='Fido', breed='Mastiff'):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed
+    
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    # name = property(get_name, set_name, get_breed, set_breed)
+
+    breed = property(get_breed, set_breed)
--- lib/testing/dog_test.py
+++ lib/testing/dog_test.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from dog import Dog
 
 import io
